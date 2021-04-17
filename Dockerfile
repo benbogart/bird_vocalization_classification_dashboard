a800af1e8f0bcd1a30b1545d8c5a7b06eaa07cfc
Ben Bogart <ben@benbogart.com>
2021-04-17 12:11:36 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.8-slim
+
+#FROM nickgryg/alpine-pandas:3.9.1
+
+WORKDIR /birdsongs
+
+COPY requirements.txt .
+
+RUN apt-get update
+RUN apt-get -y install libsndfile1 ffmpeg libavcodec-extra
+
+# RUN apk add alsa-lib-dev
+
+# RUN pip install python-dateutil pytz>=2011k
+# RUN pip --no-deps pandas
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY ./app ./app
+
+WORKDIR ./app
+CMD ["python", "./app.py"]
+# CMD ["gunicorn" "app:server"]
---
+++ app/SqueezeLayer.py
@@ -0,0 +1,30 @@
+from tensorflow import keras as K
+from tensorflow.keras.layers import Layer
+
+class SqueezeLayer(Layer):
+    '''
+    Keras squeeze layer
+    Taken from milsed https://github.com/marl/milsed/
+    '''
+    def __init__(self, axis=-1, **kwargs):
+        super(SqueezeLayer, self).__init__(**kwargs)
+        self.axis = axis
+
+    def get_output_shape_for(self, input_shape):
+        # shape = np.array(input_shape)
+        # shape = shape[shape != 1]
+        # return tuple(shape)
+        shape = list(input_shape)
+        del shape[self.axis]
+        return tuple(shape)
+
+    def compute_output_shape(self, input_shape):
+        return self.get_output_shape_for(input_shape)
+
+    def call(self, x, mask=None):
+        return K.backend.squeeze(x, axis=self.axis)
+
+    def get_config(self):
+        config = {'axis': self.axis}
+        base_config = super(SqueezeLayer, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
